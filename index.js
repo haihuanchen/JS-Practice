ecefc6287fe306d576fb61c5fe11dfad5c4452b4
github username <github email address>
2019-11-05 13:40:59 -0500

    wip

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ let movies = {
 let horrorList = document.getElementById("horror")
 let comedyList = document.getElementById("comedy")
 let actionList = document.getElementById("action")
+let formButton = document.querySelector("button[data-id='add-movie']")
+let toolsDiv = document.getElementsByClassName("tools")[0]
+
 function fetchMovies() {
     fetch("http://localhost:4000/movies")
         .then(function (resp) { return resp.json() })
@@ -33,3 +36,33 @@ function appendMovie(movie, container) {
     `
     container.append(li)
 }
+
+formButton.addEventListener("click", function (e) {
+    let form = document.createElement("form")
+    form.innerHTML = `
+        <input type="text" placeholder="title" name="title"/>
+        <input type="number" name="duration" step="0.5" min="0" placeholder="duration in hours" />
+        <input type="text" placeholder="cover" name="cover"/>
+        <input type="text" placeholder="cast member name" name="cast-name" />
+        <input type="text" placeholder="cast member character" name="cast-char" />
+        <input type="submit" value="submit"/>
+    `
+    toolsDiv.replaceChild(form, formButton)
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault()
+        let submittedForm = e.target
+        let newMovie = {
+            title: `${submittedForm.title.value}`,
+            "duration-in-hours": `${submittedForm.duration.value}`,
+            cover: `${submittedForm.cover.value}`,
+            cast: [
+                {
+                    name: `${submittedForm["cast-name"]}`,
+                    character: `${submittedForm["cast-char"]}`
+                }
+            ]
+        }
+        toolsDiv.replaceChild(formButton, form)
+    })
+})
