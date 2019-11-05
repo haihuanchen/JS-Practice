03c55d67abf43ad279a098bff4d4f06daeee8e0d
github username <github email address>
2019-11-05 13:53:15 -0500

    wip

--- index.js
+++ index.js
@@ -9,6 +9,42 @@ let actionList = document.getElementById("action")
 let formButton = document.querySelector("button[data-id='add-movie']")
 let toolsDiv = document.getElementsByClassName("tools")[0]
 
+
+function fetchHorror() {
+    fetch("http://localhost:4000/horror")
+        .then(function (resp) { return resp.json() })
+        .then(function (resp) {
+            movies.horror = resp.horror
+            movies.comedy = resp.comedy
+            movies.action = resp.action
+            movies.horror.forEach(function (movie) { appendMovie(movie, horrorList) })
+            movies.comedy.forEach(function (movie) { appendMovie(movie, comedyList) })
+            movies.action.forEach(function (movie) { appendMovie(movie, actionList) })
+        })
+}
+function fetchComedy() {
+    fetch("http://localhost:4000/comedy")
+        .then(function (resp) { return resp.json() })
+        .then(function (comedy) {
+            movies.comedy = comedy
+            movies.comedy.forEach(function (movie) { appendMovie(movie, comedyList) })
+        })
+}
+function fetchAction() {
+    fetch("http://localhost:4000/action")
+        .then(function (resp) { return resp.json() })
+        .then(function (resp) {
+            movies.horror = resp.horror
+            movies.comedy = resp.comedy
+            movies.action = resp.action
+            movies.horror.forEach(function (movie) { appendMovie(movie, horrorList) })
+            movies.comedy.forEach(function (movie) { appendMovie(movie, comedyList) })
+            movies.action.forEach(function (movie) { appendMovie(movie, actionList) })
+        })
+}
+
+
+
 function fetchMovies() {
     fetch("http://localhost:4000/movies")
         .then(function (resp) { return resp.json() })
